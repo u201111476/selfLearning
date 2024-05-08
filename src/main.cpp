7cb53299077426a752c2b2f043215a843d30477b
weijun <weij13@chinatelecom.cn>
2024-05-08 17:20:03 +0800

    feat:添加对vector的试验

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <Windows.h>
 #include <Psapi.h>
+#include <vector>
 
 #include "FileMgr.h"
 
@@ -11,10 +12,51 @@ using namespace std;
 using namespace std::chrono;		//时间相关的命名空间
 
 
+class A
+{
+public:
+	A() {
+		cout << "A's cons!" << endl; 
+		m_ptr = new char[1024 * 1024 * 4];
+	}
+	A(const A& a) {
+		cout << "A's copy cons!" << endl;
+		m_ptr = new char[1024 * 1024 * 4];
+		if (m_ptr)
+			memcpy_s(m_ptr, 1024 * 1024 * 4, a.m_ptr, 1024 * 1024 * 4);
+	}
+	A(A&& a) noexcept {	//vector容器扩容时要使用移动构造的话，移动构造函数必须是noexcept
+		cout << "A's move cons!" << endl; 
+		m_ptr = a.m_ptr;
+		a.m_ptr = nullptr;
+	}			
+	A& operator=(const A& a) {
+		cout << "A's operator=!" << endl;
+		memcpy_s(m_ptr, 1024 * 1024 * 4, a.m_ptr, 1024 * 1024 * 4);
+	}
+	~A() {
+		cout << "A's decons!" << endl;
+		delete[] m_ptr;
+	}
+private:
+	void* m_ptr{ nullptr };
+};
+
 int main()
 {
-	string str = FileMgr::GetExecutableFilePath();
-	cout << str << endl;
+	//string str = FileMgr::GetExecutableFilePath();
+	//cout << str << endl;
+	vector<A> vec;
+	//vec.reserve(20);
+	A a1;
+	vec.push_back(std::move(a1));
+	for (int i = 1; i < 10; ++i)
+	{
+		A tmp;
+		vec.emplace_back(std::move(tmp));
+		//vec.push_back(std::move(tmp));
+	}
+
 	//char buf[MAX_PATH] = {};
 	//GetModuleBaseNameA(GetCurrentProcess(), NULL, buf, MAX_PATH);
 	//cout << buf << endl;
